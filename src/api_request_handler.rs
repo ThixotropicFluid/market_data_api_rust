68f40f2abab12d46bc4fc738ebb91a30315a14d2
joel <danielewiczjoel@gmail.com>
2023-09-29 18:10:17 -0700

    moved files

--- src/api_request_handler.rs
+++ src/api_request_handler.rs
@@ -4,7 +4,47 @@ use crate::api_request_handler::request_type::RequestType;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::fmt;
+mod request_type {
+    pub trait RequestType {
+        type Data;
+        fn set_json(&mut self, json: serde_json::Value);
+        fn get_json(&self) -> Option<serde_json::Value>;
+        fn generate_request(&self) -> String;
+        fn from_json(&self) -> Option<Self::Data>;
+    }
+    pub trait RequestData {}
 
+    #[derive(Debug)]
+    pub struct StockQuote<'a> {
+        pub ticker: &'a str,
+        pub json: Option<serde_json::Value>,
+    }
+    impl RequestType for StockQuote<'_> {
+        type Data = StockQuoteData;
+        fn set_json(&mut self, json: serde_json::Value) {
+            self.json = Some(json);
+        }
+        fn get_json(&self) -> Option<serde_json::Value> {
+            None
+        }
+        fn generate_request(&self) -> String {
+            let mut res = String::from("v3/quote-short/");
+            res.push_str(self.ticker);
+            res
+        }
+        fn from_json(&self) -> Option<Self::Data> {
+            self.json.clone().unwrap();
+            let res: Vec<Self::Data> = serde_json::from_value(self.json.clone()?).unwrap();
+            res.get(0).cloned()
+        }
+    }
+    #[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+    pub struct StockQuoteData {
+        pub symbol: String,
+        pub price: f32,
+        pub volume: i32,
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod api_fetch;
 mod api_request_handler;
-mod request_type;
 
 #[cfg(test)]
 mod tests {}
